8245b932e55386af746e835fcf620158a2d6b267
Isabella Duan <isaduan001@gmail.com>
2023-01-23 21:32:37 -0500

    behold, copy pasting

--- client/src/components/pages/Messages.js
+++ client/src/components/pages/Messages.js
@@ -1,32 +1,52 @@
 import React, { useState, useEffect } from "react";
-import NavBar from "../modules/NavBar.js"
-import { post, get } from "../../utilities";
+import Card from "../modules/Card.js";
+import { NewStory } from "../modules/NewPostInput.js";
+
+import { get } from "../../utilities";
 
 import "./Messages.css";
 
 const Messages = (props) => {
     const [stories, setStories] = useState([]);
 
-    /* useEffect(() => {
-      document.title = "Message Page";
-      get("/api/messages").then((messageObjs) => {});
-    }, []); */
-
-    return(
-        <>
-            <div class="container">
-                <div class=".Messages-title">
-                    <h1>Forum</h1>
-                </div>
-
-                <div class="lines">
-                    <div class="diamond"></div>
-                </div>
-
-            </div>
-        </>
-    )
-
+    // called when the "Feed" component "mounts", i.e.
+    // when it shows up on screen
+    useEffect(() => {
+      document.title = "News Feed";
+      get("/api/stories").then((storyObjs) => {
+        let reversedStoryObjs = storyObjs.reverse();
+        setStories(reversedStoryObjs);
+      });
+    }, []);
+  
+    // this gets called when the user pushes "Submit", so their
+    // post gets added to the screen right away
+    const addNewStory = (storyObj) => {
+      setStories([storyObj].concat(stories));
+    };
+  
+    let storiesList = null;
+    const hasStories = stories.length !== 0;
+    if (hasStories) {
+      storiesList = stories.map((storyObj) => (
+        <Card
+          key={`Card_${storyObj._id}`}
+          _id={storyObj._id}
+          creator_name={storyObj.creator_name}
+          creator_id={storyObj.creator_id}
+          userId={props.userId}
+          content={storyObj.content}
+        />
+      ));
+    } else {
+      storiesList = <div>No stories!</div>;
+    }
+    return (
+      <>
+        {props.userId && <NewStory addNewStory={addNewStory} />}
+        {storiesList}
+      </>
+    );
 }
 
 export default Messages;
