b61ba9a340c7853e98950c6186d88b876869fdfc
GenericP3rson <genericp3rson@gmail.com>
2023-01-26 21:44:30 -0500

    added functions to query api on homepage

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, Component } from "react";
 import { Link } from "@reach/router";
 import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
 
@@ -6,11 +6,29 @@ import NavBar from "../modules/NavBar.js";
 import "../../utilities.css";
 import "./Skeleton.css";
 import "./pages.css";
+import { get } from "../../utilities";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "48664241058-q3dvrh1u5u8276n9h8iio1evghqf88ob.apps.googleusercontent.com";
 
 const Skeleton = ({ userId}) => {
+
+  const [events, setEvents] = useState("event")
+  const [forum, setForum] = useState("forum")
+  const [messages, setMessages] = useState("messages")
+
+    useEffect(() => {
+        get(`/api/get_events`, { }).then((userObj) => setEvents(userObj));
+      }, []);
+
+    useEffect(() => {
+        get(`/api/stories`, { }).then((userObj) => setForum(userObj));
+      }, []);
+
+    useEffect(() => {
+        get(`/api/chat`, { }).then((userObj) => setMessages(userObj));
+      }, []);
+ 
   return (
      <div className="bigContainer">
       {userId ?  <div className="HomeContainer">
@@ -18,16 +36,25 @@ const Skeleton = ({ userId}) => {
           <Link to="/messages/" className="Title">
               Messages
           </Link>
+          <div>
+            {messages[messages.length-1].content}  @IZZY — EDIT THIS LINE!!   
+          </div>
         </div>
         <div className="forumColumn">
           <Link to="/forum/" className="Title">
                 Forum
             </Link>
+          <div>
+            {forum[forum.length-1].content}  @IZZY — EDIT THIS LINE!!   
+          </div>
         </div>
         <div className="eventsColumn">
           <Link to="/calendar/" className="Title">
                 Events
-            </Link>
+          </Link>
+          <div>
+            {events[0].title}  @IZZY — EDIT THIS LINE!! 
+          </div>
           <div className="recommendationsColumn"> 
             <Link to="/findgroup/" className="Title">
                   Recommendations
@@ -45,59 +72,5 @@ const Skeleton = ({ userId}) => {
 
 export default Skeleton;
 
-// import React from "react";
-// import { Link } from "@reach/router";
-// import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
 
-// import NavBar from "../modules/NavBar.js";
-// import "../../utilities.css";
-// import "./Skeleton.css";
-// import "./pages.css";
 
-// //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-// const GOOGLE_CLIENT_ID = "FILL ME IN";
-
-// const Skeleton = ({ userId, handleLogin, handleLogout }) => {
-//   return (
-//     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-//       <NavBar/>
-//       {userId ? (
-//         <button
-//           onClick={() => {
-//             googleLogout();
-//             handleLogout();
-//           }}
-//         >
-//           Logout
-//         </button>
-//       ) : (
-//         <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
-//       )}   
-//       <div class="HomeContainer">
-//           <div class="messagesColumn">
-//             <Link to="/messages/" className="Title">
-//                 Messages
-//             </Link>
-//           </div>
-//           <div class="forumColumn">
-//             <Link to="/forum/" className="Title">
-//                   Forum
-//             </Link>
-//           </div>
-//           <div class="messagesColumn">
-//             <div class="eventsColumn">
-//               <Link to="/calendar/" className="Title">
-//                   Events
-//               </Link>
-//             </div>
-//             <div class="recommendationsColumn"> 
-//               <div class="Title">Connections</div>
-//             </div>
-//           </div>
-//       </div>
-
-//     </GoogleOAuthProvider>
-//   );
-// };
-
-// export default Skeleton;
